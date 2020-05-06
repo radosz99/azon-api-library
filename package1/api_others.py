e31c95228c17628a6e8781253f4ce2481a9af25d
radosz99 <47248979+radosz99@users.noreply.github.com>
2020-05-06 17:06:52 +0200

    Add get_pwr_research_centres method

--- package1/api_others.py
+++ package1/api_others.py
@@ -14,3 +14,38 @@ def get_programming_languages(api_key):
 
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
+
+class ResearchCenters:
+    def __init__(self,_id, name, number, director, tel, email, webpage):
+        self.id=_id
+        self.name=name
+        self.number=number
+        self.director=director
+        self.tel=tel
+        self.email=email
+        self.webpage=webpage
+
+    def __repr__(self):
+        return f'Id - {self.id}\nNazwa - {self.name}\nNumer - {self.number}\nDyrektor - {self.director}\nTelefon - {self.tel}\nE-mail - {self.email}\nStrona - {self.webpage}\n'
+
+def get_pwr_reseach_centres(api_key):
+    try:
+        response = requests.get("https://api.e-science.pl/api/azon/databases/pwr_research_centers/", headers={'X-Api-Key': api_key})
+        response.raise_for_status()
+        json_data  = response.json()
+        results = json_data['results']
+        rc_list=[]
+        for item in results:
+            info={}
+            info['_id']=item['id']
+            info['name']=item['name']
+            info['number']=item['number']
+            info['director']=item['director']
+            info['tel']=item['tel']
+            info['email']=item['email']
+            info['webpage']=item['webpage']
+            rc_list.append(ResearchCenters(**info))
+        return rc_list
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
--- package1/test.py
+++ package1/test.py
@@ -6,7 +6,7 @@ api_key='JhHl8LOiMRCg3EFWnRcEpxdXmz1VJDYOc0fEhtBY'
 #entries = api_handler.get_entries_by_page(api_key,100)
 #etails = api_handler.get_entries_details(entries,api_key)
 
-entries = api_others.get_programming_languages(api_key)
+entries = api_others.get_pwr_reseach_centres(api_key)
 for detail in entries:
     print(detail)
 # for item in details:
